c2b4b447bc65381eda6f92579114777f5f6cb76a
bdcorps <bdcorps@gmail.com>
2018-08-09 15:09:26 -0400

    add category, playliylists mongo model

---
+++ uploads/categoriesConfig.js
@@ -0,0 +1,50 @@
+var categoriesJSON = {
+    categories: [{
+        id: "topheadlines",
+        title: "Top Headlines",
+        playlists: [{
+            type: "topHeadlines",
+            query: {
+                q: 'bitcoin',
+                language: 'en',
+                'pageSize': 10
+            }
+        },
+        {
+            type: "topHeadlines",
+            query: {
+                category: 'business',
+                language: 'en',
+                'pageSize': 10
+            }
+        }
+        ]
+    },
+    {
+        id: "dailynews",
+        title: "Daily News",
+        playlists: [{
+            type: "topHeadlines",
+            query: {
+                sources: 'bbc-news,the-verge',
+                q: 'bitcoin',
+                language: 'en',
+                'pageSize': 10
+            }
+        },
+        {
+            type: "topHeadlines",
+            query: {
+                category: 'business',
+                language: 'en',
+                'pageSize': 10
+            }
+        }
+        ]
+    }
+
+    ]
+}
+
+
+module.exports = categoriesJSON;
