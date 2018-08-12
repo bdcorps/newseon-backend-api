86e1b7ebb0dda52dee231f63be070154de2b4b31
bdcorps <bdcorps@gmail.com>
2018-08-12 19:33:24 -0400

    add docs; add new GET endpoints to retrieve Categories, Playlists and Article data

--- uploads/categoriesConfig.js
+++ uploads/categoriesConfig.js
@@ -17,3 +17,54 @@ var categoriesJSON = {
 
 
 module.exports = categoriesJSON;
+
+// var categoriesJSON = {
+//     categories: [{
+//         id: "topheadlines",
+//         title: "Top Headlines",
+//         playlists: [{
+//             type: "topHeadlines",
+//             query: {
+//                 q: 'bitcoin',
+//                 language: 'en',
+//                 'pageSize': 10
+//             }
+//         },
+//         {
+//             type: "topHeadlines",
+//             query: {
+//                 category: 'business',
+//                 language: 'en',
+//                 'pageSize': 10
+//             }
+//         }
+//         ]
+//     },
+//     {
+//         id: "dailynews",
+//         title: "Daily News",
+//         playlists: [{
+//             type: "topHeadlines",
+//             query: {
+//                 sources: 'bbc-news,the-verge',
+//                 q: 'bitcoin',
+//                 language: 'en',
+//                 'pageSize': 10
+//             }
+//         },
+//         {
+//             type: "topHeadlines",
+//             query: {
+//                 category: 'business',
+//                 language: 'en',
+//                 'pageSize': 10
+//             }
+//         }
+//         ]
+//     }
+
+//     ]
+// }
+
+
+// module.exports = categoriesJSON;
