a9ea83bfc69f0c5dde57204e930252283e89f14e
bdcorps <bdcorps@gmail.com>
2018-08-12 15:19:23 -0400

    add categories, playlists and articles upload to db support

--- models/models.js
+++ models/models.js
@@ -30,7 +30,7 @@ PlaylistSchema.statics.findOrCreate = require("find-or-create");
 CategorySchema.statics.findOrCreate = require("find-or-create");
 
 var ArticleModel = mongoose.model('article', ArticleSchema, 'articles');
-var PlaylistModel = mongoose.model('playlist', CategorySchema, 'playlists');
+var PlaylistModel = mongoose.model('playlist', PlaylistSchema, 'playlists');
 var CategoryModel = mongoose.model('category', CategorySchema, 'categories');
 
 module.exports = { ArticleModel: ArticleModel, PlaylistModel: PlaylistModel, CategoryModel: CategoryModel };
